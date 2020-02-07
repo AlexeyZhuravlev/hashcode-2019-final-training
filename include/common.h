5f646facfbfbbc96b9b55cd42949b7edcfd67714
Алексей <shkiper@MacBook-Aleksej.local>
2020-02-07 19:33:13 +0300

    first commit

---
+++ include/common.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <string>
+#include <queue>
+#include <set>
+#include <map>
+#include <cstdlib>
+#include <unordered_map>
+
+using namespace std;
+
+struct Context {
+    using TSolution = void;
+    TSolution solution;
+
+    void Input() {
+
+    }
+
+
+    void Output() {
+    
+    }
+
+    void Verify() {
+        
+    }
+
+    int GetScore() {
+        return 0;
+    }
+
+    void Better(const int iterations = 1000, const int generation = 100) {
+
+    }
+
+};
